718d3aafe0a5ffe743d1baf5017e9f51b7552fce
Dean Biele <dean@invisionlearning.com>
2016-01-13 14:43:25 -0500

    adding sample jasmine test

---
+++ CODE/scripts/tests/unit/app/components/jasmineunit.spec.ts
@@ -0,0 +1,14 @@
+﻿import {it, describe, expect} from 'angular2/testing';
+import {normalizeDataTest} from '../../../../app/components/jasmineunit';
+describe("normalizeData tests", function () {
+    // First argument to "it" is the name of the specific test
+    it("accepts golden path data", function () {
+        // Use the unit being tested as necessary
+        var json = '{"Name": "Maria", "PersonalIdentifier": 2111858}';
+        var norm = normalizeDataTest(json);
+
+        // Check the results; "expect" is a Jasmine method.
+        expect(norm.name).toEqual("Maria");
+        expect(norm.id).toEqual(2111858);
+    });
+});
