26cc942f4acbfd0d42a216e1008c91d83e0d2617
Dean Biele <dean@invisionlearning.com>
2016-01-18 15:02:21 -0500

    added arrow functions.

--- CODE/scripts/tests/unit/app/components/jasmineunit.spec.ts
+++ CODE/scripts/tests/unit/app/components/jasmineunit.spec.ts
@@ -1,14 +1,15 @@
-﻿import {it, describe, expect} from 'angular2/testing';
+﻿// Code is borrowed from http://blogs.msdn.com/b/visualstudio/archive/2016/01/11/unit-testing-apache-cordova-apps-with-visual-studio-part-1.aspx 
+
+import {it, describe, expect} from 'angular2/testing';
 import {normalizeDataTest} from '../../../../app/components/jasmineunit';
-describe("normalizeData tests", function () {
+describe('normalizeData tests', () => {
     // First argument to "it" is the name of the specific test
-    it("accepts golden path data", function () {
+    it('Accepts golden path data', () => {
         // Use the unit being tested as necessary
         var json = '{"Name": "Maria", "PersonalIdentifier": 2111858}';
         var norm = normalizeDataTest(json);
-
-        // Check the results; "expect" is a Jasmine method.
-        expect(norm.name).toEqual("Maria");
+        //Check the results; "expect" is a Jasmine method.
+        expect(norm.name).toEqual('Maria');
         expect(norm.id).toEqual(2111858);
     });
 });
