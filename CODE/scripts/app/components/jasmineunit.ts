ebd883e53a265d7624f6098ace5dff14703ee7bd
Dean Biele <dean@invisionlearning.com>
2016-01-18 15:02:58 -0500

    adding to repository

---
+++ CODE/scripts/app/components/jasmineunit.ts
@@ -0,0 +1,12 @@
+﻿interface UserData {
+    Name: string;
+    PersonalIdentifier: string;
+}
+
+export function normalizeDataTest(jsonIn) {
+    var data: UserData = JSON.parse(jsonIn);
+    return {
+        name: data.Name,
+        id: Number(data.PersonalIdentifier)
+    };
+}
