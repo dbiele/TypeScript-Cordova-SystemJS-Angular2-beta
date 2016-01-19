2d5f45c32c2040cd3a608a6498925e3fc8405fb8
Dean Biele <dean@invisionlearning.com>
2016-01-19 18:48:38 -0500

    updated formatting and included let

--- CODE/scripts/tests/unit/app/components/jasmineunit.spec.ts
+++ CODE/scripts/tests/unit/app/components/jasmineunit.spec.ts
@@ -1,15 +1,20 @@
-﻿// Code is borrowed from @kraigbro http://blogs.msdn.com/b/visualstudio/archive/2016/01/11/unit-testing-apache-cordova-apps-with-visual-studio-part-1.aspx 
+﻿// Borrowed from @kraigbro 
+// http://blogs.msdn.com/b/visualstudio/archive/2016/01/11/unit-testing-apache-cordova-apps-with-visual-studio-part-1.aspx 
 
 import {it, describe, expect} from 'angular2/testing';
+
 import {normalizeDataTest} from '../../../../app/components/jasmineunit';
 describe('normalizeData tests', () => {
-    // First argument to "it" is the name of the specific test
+    /* tslint:disable:no-console */
+    // First argument to "it" = name of the specific test that appears in the report.
+    console.log('testing');
     it('Accepts golden path data', () => {
         // Use the unit being tested as necessary
-        var json = '{"Name": "Maria", "PersonalIdentifier": 2111858}';
-        var norm = normalizeDataTest(json);
+        let json: string = '{"Name": "Maria", "PersonalIdentifier": 2111858}';
+        let norm = normalizeDataTest(json);
         //Check the results; "expect" is a Jasmine method.
         expect(norm.name).toEqual('Maria');
         expect(norm.id).toEqual(2111858);
     });
+    /* tslint:enable:no-console */
 });
