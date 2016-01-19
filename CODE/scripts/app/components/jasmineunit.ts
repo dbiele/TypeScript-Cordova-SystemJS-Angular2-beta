ef6a06459f035cf724f418520549a1ead9d40ed6
Dean Biele <dean@invisionlearning.com>
2016-01-19 18:48:06 -0500

    stop using var.  Use let.

--- CODE/scripts/app/components/jasmineunit.ts
+++ CODE/scripts/app/components/jasmineunit.ts
@@ -3,8 +3,8 @@
     PersonalIdentifier: string;
 }
 
-export function normalizeDataTest(jsonIn) {
-    var data: UserData = JSON.parse(jsonIn);
+export function normalizeDataTest(jsonIn: string) {
+    let data: UserData = JSON.parse(jsonIn);
     return {
         name: data.Name,
         id: Number(data.PersonalIdentifier)
