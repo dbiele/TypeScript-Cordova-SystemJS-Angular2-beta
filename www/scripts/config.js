7aa672e0a9dbbf25df766377f28b0ff13f449215
Dean Biele <dean@invisionlearning.com>
2016-01-20 21:57:40 -0500

    adding to repo. travis ci update

---
+++ www/scripts/config.js
@@ -0,0 +1,11 @@
+System.config({
+    baseURL: './scripts',
+    defaultJSExtensions: true,
+    transpiler: 'none',
+    map: {
+        howler: './scripts/lib/howler/howler.js'
+        //webgl_heatmap: './scripts/webgl-heatmap.js',
+        //d3: './node_modules/d3/d3.js'
+    }
+
+});
