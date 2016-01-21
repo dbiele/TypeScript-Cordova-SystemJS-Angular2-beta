a6699898d6a93f0bc58fc863d89f1c3dc3f8ef28
Dean Biele <dean@invisionlearning.com>
2016-01-20 21:47:26 -0500

    updating folder structure for travis ci

--- CODE/scripts/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'test-app',
-    template: '<h1>My First Angular 2 App</h1>'
-})
-export class AppComponent {
-
-}
--- CODE/scripts/typings/tsd.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="systemjs/systemjs.d.ts" />
-/// <reference path="howlerjs/howler.d.ts" />
