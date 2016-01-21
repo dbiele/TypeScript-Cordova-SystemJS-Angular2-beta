1b0f68b873cbf24f8e3639fb4bcfbdeaa9c38084
Dean Biele <dean@invisionlearning.com>
2016-01-20 20:36:00 -0500

    updated to include promise to allow additional imports

--- CODE/scripts/start.ts
+++ CODE/scripts/start.ts
@@ -1,11 +1,20 @@
 // Embedded in index.html
 
-function bootstrapApp(m: any) {
-    m.initialize();
+/**
+ * 
+ * @param m = array of modules.
+ * The last module is the array is the imported index module.  See index.ts
+ */
+function bootstrapApp(m: Array<any>) {
+    m[m.length - 1].initialize();
 }
-// Example code to load jquery using systemjs
-// System.import('lib/jquery/jquery.js');
-System.import('index')
+
+Promise.all([
+    // Add as many System.imports as necessary. 
+    // Position of system.import('index') is important. The returned module array is used the bootstrapApp function above.
+    System.import('howler'),
+    System.import('index')
+])
     .catch((error: Object) => console.error('Error: index loading with System:', error.toString()))
     .then(bootstrapApp)
     .catch((error: Object) => console.error('Error: Boostrapping', error.toString()));
