c274b26d1ce8665014b4f7d145fd6634b2dff067
Dean Biele <dean@invisionlearning.com>
2016-01-20 20:36:36 -0500

    Updated comments and added howler test.

--- CODE/scripts/app.ts
+++ CODE/scripts/app.ts
@@ -1,13 +1,18 @@
 // import {enableDebugTools} from 'angular2/platform/browser';
-// Used to demonstrate external module is loading and function is called.
+// Used to demonstrate external module is loading and the function is called.
 
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app/app.component';
+import {AudioHandler} from './app/audio/audiohandler';
 
 export function appExternalModuleTest(): void {
-    console.log('testmodules called in testExternalModules');
+    // Bootstrap Angular2 
     bootstrap(AppComponent);
-    
+
+    // Test if Howler has loaded correctly by playing audio.
+    let testAudio: AudioHandler = new AudioHandler();
+    testAudio.playAudio(['../audio/audiocheck.net_linearfrequencytest.mp3', '../audio/audiocheck.net_linearfrequencytest.ogg']);
+
     //enable angular2 debug tools
     //bootstrap(AppComponent).then((appRef) => { enableDebugTools(appRef); });
 }
