e46bb7131f7a3393dff1bb6086d9a95dbe66c506
Dean Biele <dean@invisionlearning.com>
2016-01-22 12:40:53 -0500

    adding to repo

---
+++ tools/protractor.conf.js
@@ -0,0 +1,32 @@
+﻿// more info about reflect-metadata
+// https://www.npmjs.com/package/reflect-metadata
+require('reflect-metadata');
+
+exports.config = {
+    framework: 'jasmine2',
+    baseUrl: 'http://localhost:8080/',
+
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['show-fps-counter=true']
+        }
+    },
+    directConnect: true,
+
+    allScriptsTimeout: 110000,
+    getPageTimeout: 100000,
+    jasmineNodeOpts: {
+        isVerbose: false,
+        showColors: true,
+        includeStackTrace: false,
+        defaultTimeoutInterval: 400000
+    },
+
+    /**
+	 * Angular2 related configuration
+	 * `useAllAngular2AppRoots` tells Protractor to wait for any Angular 2 apps
+	 * on the page instead of just the one matching `rootEl`
+	 */
+    useAllAngular2AppRoots: true
+};
