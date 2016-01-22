02acf851a5118a4cc2699a4026f160e0a492132e
Dean Biele <dean@invisionlearning.com>
2016-01-22 12:42:48 -0500

    adding files to repo

---
+++ scripts/tests/e2e/app/app.components.spec.ts
@@ -0,0 +1,37 @@
+﻿//import {verifyNoBrowserErrors} from 'angular2/src/testing/e2e_util';
+import {it, describe, expect, beforeEach} from 'angular2/testing';
+
+describe('Text on screen', function () {
+
+    //afterEach(verifyNoBrowserErrors);
+    //beforeEach(() => {
+    //    browser.get('/');
+    //});
+
+    //describe('hello world app', function () {
+    //    var URL = 'scripts/app/app.component.html';
+    //    console.log('angular e2e testing');
+    //    it('should greet', function () {
+    //        browser.get(URL);
+
+    //        expect(getComponentText('test-app', '.title')).toEqual('Angular2');
+    //    });
+
+    //    it('should change greeting', function () {
+    //        browser.get(URL);
+
+    //        expect(getComponentText('test-app', '.title')).toEqual('howdy world!');
+    //    });
+    //});
+
+});
+
+//function getComponentText(selector, innerSelector) {
+//    return browser.executeScript('return document.querySelector("' + selector + '").querySelector("' +
+//        innerSelector + '").textContent');
+//}
+
+//function clickComponentButton(selector, innerSelector) {
+//    return browser.executeScript('return document.querySelector("' + selector + '").querySelector("' +
+//        innerSelector + '").click()');
+//}
