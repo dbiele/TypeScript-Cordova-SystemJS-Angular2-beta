503c99357bb7b60345f8b31b2b927641a643ad6a
Dean Biele <dean@invisionlearning.com>
2016-01-26 19:35:23 -0500

    problem with my protractor.spec files and system imports.  Still figuring it out.

---
+++ scripts/tests/e2e/app/problem.ts
@@ -0,0 +1,3 @@
+﻿// I don't know why, but an import needs to be in this folder to prevent errors with protractor
+// Adding a systemjs import in the spec.ts file seems to break because of the system.register
+import {it, describe, expect, beforeEach} from 'angular2/testing';
