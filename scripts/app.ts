463f7ec480cdafc5ab0319b9ec9379bd4c49f422
Dean Biele <dean@invisionlearning.com>
2016-01-30 18:17:21 -0500

    added hockeyapp and soundjs

--- scripts/app.ts
+++ scripts/app.ts
@@ -4,15 +4,23 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app/app.component';
 import {AudioHandler} from './app/audio/audiohandler';
+import {HockeyAppServices} from './util/hockeyappservices';
 
+/**
+ * Fires when the cordova deviceready event is fired.
+ * Or, when cordova does not exist and deviceready is automatically called.
+ */
 export function appExternalModuleTest(): void {
+    console.log('play audio');
     // Bootstrap Angular2 
     bootstrap(AppComponent);
-
-    // Test if Howler has loaded correctly by playing audio.
+    console.log('now play audio');
+    // Initialize soundJS
     let testAudio: AudioHandler = new AudioHandler();
-    testAudio.playAudio(['../audio/audiocheck.net_linearfrequencytest.mp3', '../audio/audiocheck.net_linearfrequencytest.ogg']);
-
+    testAudio.playAudio(['./www/audio/sound.mp3']);
+    //console.log('playing audio done');
     //enable angular2 debug tools
     //bootstrap(AppComponent).then((appRef) => { enableDebugTools(appRef); });
+
+    let hockeyapp = new HockeyAppServices();
 }
