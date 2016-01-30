0dddbfc50f59006f65c5c3c7ba3d454369f0414e
Dean Biele <dean@invisionlearning.com>
2016-01-30 18:16:48 -0500

    adding soundjs

--- scripts/start.ts
+++ scripts/start.ts
@@ -12,7 +12,8 @@ function bootstrapApp(m: Array<any>) {
 Promise.all([
     // Add as many System.imports as necessary. 
     // Position of system.import('index') is important. The returned module array is using the bootstrapApp() function above.
-    System.import('howler'),
+    // The soundJS location is defined in the ./www/scripts/config.js file or you can use a url location ie. ../scripts/test.
+    System.import('soundJS'),
     System.import('index')
 ])
     .catch((error: Object) => console.error('Error: index loading with System:', error.toString()))
