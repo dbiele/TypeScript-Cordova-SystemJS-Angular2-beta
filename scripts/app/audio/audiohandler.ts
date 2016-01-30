45b781a04fdcbff4686c6b0205f276990d69c77a
Dean Biele <dean@invisionlearning.com>
2016-01-30 18:18:38 -0500

    added soundjs

--- scripts/app/audio/audiohandler.ts
+++ scripts/app/audio/audiohandler.ts
@@ -1,13 +1,30 @@
 ﻿/**
  * This class is used to test systemjs and it's ability to import external .js files.
- * Howler is loaded in start.ts and used here. System.import('howler')
- * Info = http://goldfirestudios.com/blog/104/howler.js-Modern-Web-Audio-Javascript-Library
+ * SoundJS is loaded in start.ts and used here. System.import('soundjs')
+ * Info = http://www.createjs.com/soundjs
  */
 export class AudioHandler {
+    constructor() {
+        //, createjs.CordovaAudioPlugin
+        createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.HTMLAudioPlugin, createjs.CordovaAudioPlugin]);
+        createjs.Sound.alternateExtensions = ['mp3'];
+        createjs.Sound.on("fileload", this.loadHandler, this);
+        
+    }
 
     public playAudio(audioFiles: Array<string>): void {
-        let sound = new Howl({
-            urls: audioFiles
-        }).play();
+        createjs.Sound.registerSound('audio/sound.mp3', 'sound');
+    }
+
+    private loadHandler(event) {
+        // This is fired for each sound that is registered.
+        console.log('play audio');
+        let instance = createjs.Sound.play('sound');  // play using id.  Could also use full source path or event.src.
+        instance.on("complete", this.handleComplete, this);
+        instance.volume = 0.5;
+    }
+
+    private handleComplete(event) {
+        console.log('handle complete');
     }
 }
