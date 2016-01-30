214605398baa03050390749b4b30b2885d4af644
Dean Biele <dean@invisionlearning.com>
2016-01-30 18:17:44 -0500

    adding soundjs

--- scripts/typings/tsd.d.ts
+++ scripts/typings/tsd.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="systemjs/systemjs.d.ts" />
-/// <reference path="howlerjs/howler.d.ts" />
+/// <reference path="createjs-lib/createjs-lib.d.ts" />
+/// <reference path="preloadjs/preloadjs.d.ts" />
+/// <reference path="soundjs/soundjs.d.ts" />
