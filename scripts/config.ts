683f07373099a6248d32cf1d5710bb759238506e
Dean Biele <dean@invisionlearning.com>
2016-01-31 10:49:28 -0500

    adding config.ts instead of using config.js in /www/scripts folder.

---
+++ scripts/config.ts
@@ -0,0 +1,12 @@
+﻿System.config({
+    baseURL: './scripts',
+    defaultJSExtensions: true,
+    transpiler: 'none',
+    map: {
+        // The js files need to be located in the root /www/ folder.
+        // For this project, the scripts are located in /www/scripts/lib
+        soundJS: './scripts/lib/createjs/soundjs-0.6.2.combined.js'
+        //webgl_heatmap: './scripts/webgl-heatmap.js',
+        //d3: './node_modules/d3/d3.js'
+    }
+});
