1a666e75d327842087b084929dc7db07d32fea6c
Dean Biele <dean@invisionlearning.com>
2016-01-30 19:37:23 -0500

    formatting

--- scripts/app/audio/audiohandler.ts
+++ scripts/app/audio/audiohandler.ts
@@ -8,23 +8,25 @@ export class AudioHandler {
         //, createjs.CordovaAudioPlugin
         createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.HTMLAudioPlugin, createjs.CordovaAudioPlugin]);
         createjs.Sound.alternateExtensions = ['mp3'];
-        createjs.Sound.on("fileload", this.loadHandler, this);
-        
+        createjs.Sound.on('fileload', this.loadHandler, this);
+
     }
 
     public playAudio(audioFiles: Array<string>): void {
-        createjs.Sound.registerSound('audio/sound.mp3', 'sound');
+        // this is just for testing.
+        createjs.Sound.registerSound(audioFiles[0], 'soundID');
     }
 
-    private loadHandler(event) {
+    private loadHandler(event: Event) {
+        console.log('this is a test' + event);
         // This is fired for each sound that is registered.
         console.log('play audio');
-        let instance = createjs.Sound.play('sound');  // play using id.  Could also use full source path or event.src.
-        instance.on("complete", this.handleComplete, this);
+        let instance = createjs.Sound.play('soundID');  // play using id.  Could also use full source path or event.src.
+        instance.on('complete', this.handleComplete, this);
         instance.volume = 0.5;
     }
 
-    private handleComplete(event) {
+    private handleComplete(event: Event) {
         console.log('handle complete');
     }
 }
