74ba83f7b54b39ed553f2cd7788b5156edc5b8d9
Dean Biele <dean@invisionlearning.com>
2016-02-09 18:54:39 -0500

    adding typings

--- scripts/typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="systemjs/systemjs.d.ts" />
-/// <reference path="createjs-lib/createjs-lib.d.ts" />
-/// <reference path="preloadjs/preloadjs.d.ts" />
-/// <reference path="soundjs/soundjs.d.ts" />
