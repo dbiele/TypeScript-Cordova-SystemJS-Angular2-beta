169e0d5a9c54b40a60291d72e02161c9f681757f
Dean Biele <dean@invisionlearning.com>
2016-02-10 15:54:56 -0500

    tslint cleanup

--- scripts/tests/e2e/e2e_util.ts
+++ scripts/tests/e2e/e2e_util.ts
@@ -1,23 +1,24 @@
 import * as webdriver from 'selenium-webdriver';
+let globalKey: string = 'browser';
+let global$: string = '$';
+export var browser: protractor.IBrowser = global[globalKey];
+export var $: cssSelectorHelper = global[global$];
 
-export var browser: protractor.IBrowser = global['browser'];
-export var $: cssSelectorHelper = global['$'];
-
-export function clickAll(buttonSelectors) {
-  buttonSelectors.forEach(function(selector) { $(selector).click(); });
+export function clickAll(buttonSelectors: any) {
+    buttonSelectors.forEach(function (selector: any) { $(selector).click(); });
 }
 
 export function verifyNoBrowserErrors() {
-  // TODO(tbosch): Bug in ChromeDriver: Need to execute at least one command
-  // so that the browser logs can be read out!
-  browser.executeScript('1+1');
-  browser.manage().logs().get('browser').then(function(browserLog) {
-    var filteredLog = browserLog.filter(function(logEntry) {
-      if (logEntry.level.value >= webdriver.logging.Level.INFO.value) {
-        console.log('>> ' + logEntry.message);
-      }
-      return logEntry.level.value > webdriver.logging.Level.WARNING.value;
+    // TODO(tbosch): Bug in ChromeDriver: Need to execute at least one command
+    // so that the browser logs can be read out!
+    browser.executeScript('1+1');
+    browser.manage().logs().get('browser').then(function (browserLog: any) {
+        var filteredLog = browserLog.filter(function (logEntry: any) {
+            if (logEntry.level.value >= webdriver.logging.Level.INFO.value) {
+                console.log('>> ' + logEntry.message);
+            }
+            return logEntry.level.value > webdriver.logging.Level.WARNING.value;
+        });
+        expect(filteredLog.length).toEqual(0);
     });
-    expect(filteredLog.length).toEqual(0);
-  });
 }
