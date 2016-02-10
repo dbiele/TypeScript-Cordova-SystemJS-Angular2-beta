61b2fa15952a81c6b2fb8e9cf3fd66ce4debb441
Dean Biele <dean@invisionlearning.com>
2016-02-10 08:11:31 -0500

    Adding new typings

---
+++ scripts/typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser\ambient\createjs-lib\createjs-lib.d.ts" />
+/// <reference path="browser\ambient\preloadjs\preloadjs.d.ts" />
+/// <reference path="browser\ambient\soundjs\soundjs.d.ts" />
+/// <reference path="browser\ambient\systemjs\systemjs.d.ts" />
---
+++ scripts/typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main\ambient\createjs-lib\createjs-lib.d.ts" />
+/// <reference path="main\ambient\preloadjs\preloadjs.d.ts" />
+/// <reference path="main\ambient\soundjs\soundjs.d.ts" />
+/// <reference path="main\ambient\systemjs\systemjs.d.ts" />
